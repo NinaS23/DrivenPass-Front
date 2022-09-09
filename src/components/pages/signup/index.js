4f5c2d07d11c5c803f70e8536b192b03733532b6
MariaDuda <mariabevenuto062@gmail.com>
2022-09-09 16:31:54 -0300

    feat cadastro

--- src/components/pages/signup/index.js
+++ src/components/pages/signup/index.js
@@ -2,24 +2,50 @@ import { MainContent, Title, Inputs } from "./style";
 import lock from "../../../assets/images/lock.svg";
 import FormsLoginSingin from "../../shared/FormsLoginSigin";
 import { useState } from "react";
-
-
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import swal from "sweetalert";
 
 export default function Signup(){
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [disabled, setDisabled] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate()
+
+    async function sendFormes(e) {
+        try {
+          e.preventDefault();
+          setDisabled(true);
+          setLoading(true)
+    
+          const body = { email, password };
+    
+          await axios.post(`http://localhost:6003/sign-up`, body);
+    
+          navigate("/");
+        } catch (error) {
+          setDisabled(false);
+          setLoading(false)
+          swal(error.response.data , error.response.data , "error")
+        }
+      }
+
     return(
         <MainContent>
         <div>
             <img src={lock} alt="cadeado" />
         </div>
-        <Title>DrivenPass</Title>
-        <Inputs>
-            <FormsLoginSingin
-                type={true}
-                email={email}
-                setEmail={setEmail}
-                password={password}
+            <Title>DrivenPass</Title>
+            <Inputs>
+                <FormsLoginSingin
+                    type={true}
+                    email={email}
+                    loading={loading}
+                    disabled={disabled}
+                    sendFormes={sendFormes}
+                    setEmail={setEmail}
+                    password={password}
                     setPassword={setPassword}
                     typeRegister={"register"}
                     LinkTo={"/"}
