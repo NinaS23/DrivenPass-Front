8cb1d575005e77c8ae33f13258c6852a4e4482c8
MariaDuda <mariabevenuto062@gmail.com>
2022-09-09 15:12:03 -0300

    fix footer

--- src/components/pages/credentials/credentialById.js
+++ src/components/pages/credentials/credentialById.js
@@ -95,5 +95,11 @@ color: #000000;
 
 const Footer = styled.div`
 display: flex;
-
+position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: #FFFFFF;
+  color: white;
+  text-align: center;
 `
--- src/components/pages/safeNotes/safeNoteById.js
+++ src/components/pages/safeNotes/safeNoteById.js
@@ -92,5 +92,12 @@ color: #000000;
 
 const Footer = styled.div`
 display: flex;
+position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: #FFFFFF;
+  color: white;
+  text-align: center;
 
 `
